d472ec3d4da6031032f0a15d42281c8c2391a7df
KEN RONOH <kennethronoh1@gmail.com>
2017-06-05 00:32:36 -0700

    Create render.php

---
+++ render.php
@@ -0,0 +1,22 @@
+ <?php require_once('auth.php');   include ('buffer.php');?>
+ 
+ 
+ 
+ <?php do { ?> 
+ 
+ 
+ 
+ 
+<li><span><i class="fa fa-home"></i> <?php echo $row_dev['acres']; ?>  Sq ft</span></li> // buffers items you need to render
+
+
+
+
+
+  <?php } while ($row_dev = mysql_fetch_assoc($dev)); ?>
+
+
+		
+          
+       
+               
